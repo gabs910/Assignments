5bf29fe24916268349c9768a25f09bccf3e807a2
Gabs910 <gabham910@hotmail.com>
2016-08-19 13:54:19 -0400

    RPN calculator second submit, using stack this time.

--- Day4/RPNCalculator.java
+++ Day4/RPNCalculator.java
@@ -1,42 +1,65 @@
 package ssa;
 
 import java.util.Scanner;
-// main class declaration
+import java.util.Stack;
+
+//class declaration
 public class RPNCalculator {
-	
+
+	private static Scanner z;
 
 	public static void main(String[] args) {
-		int solution = 0;
-		Scanner z = new Scanner(System.in);
-//prompts for user to enter parameters for equation
-		System.out.println("Enter a number:");
-		int x = z.nextInt();
-		
-		System.out.println("Enter another number:");
-		int y = z.nextInt();
-		
-		System.out.println("Enter the operation you would like to perform:");
-		String operand = z.next();
-		
-		System.out.println("You would like to solve:");
-		
-		System.out.print(x); System.out.print(y); System.out.println(operand);
-//switch statement to handle the operand as a string		
-		switch(operand){
-		
-			case "+": solution = x + y;
-			break;
-		
-			case "-": solution = x - y;
-			break;
-			
-			case "/": solution = x / y;
-			break;
-			
-			case "*": solution = x * y;
-			break;
-		}
-		System.out.println("Your solution is " + solution);
-	}
+		z = new Scanner(System.in);
+		Stack<Integer> stk = new Stack<Integer>();
+		boolean quit = false;
+		boolean firstTime = true;
 
+		while (!quit) {
+//get first number from user and push to the stack			
+			System.out.println("Enter a number:");
+			int x = z.nextInt();
+			stk.push(x);
+//if loop checks to see if this is the first time the while loop runs through.
+			if (firstTime) {
+				firstTime = false;
+//get another number from the user				
+				System.out.println("Enter another number:");
+				int y = z.nextInt();
+				stk.push(y);
+			}
+//get operator for equation from user			
+			System.out.println("Enter the operation you would like to perform:");
+			String operand = z.next();
+//pop off of the stack into the variables
+			int pop1 = stk.pop();
+			int pop2 = stk.pop();
+//switch checks to see which operator was chosen in order to run the correct equation
+			switch (operand) {
+
+			case "+":
+				stk.push(pop2 + pop1);
+				break;
+
+			case "-":
+				stk.push(pop2 - pop1);
+				break;
+
+			case "/":
+				stk.push(pop2 / pop1);
+				break;
+
+			case "*":
+				stk.push(pop2 * pop1);
+				break;
+			}
+			System.out.print(pop2); System.out.print(pop1); System.out.println(operand);
+			System.out.println("Do you want to continue? Y or N: ");
+			String stop = z.next();
+			if (stop.equalsIgnoreCase("N")) {
+				quit = true;
+			}
+
+		}
+		System.out.println("Your solution is " + stk.pop());
+	}
 }
