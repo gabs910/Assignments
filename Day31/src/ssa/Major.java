a2b46aef5559685e71f6661d28d2aed93204abb5
Gabs910 <gabham910@hotmail.com>
2016-09-26 16:22:05 -0400

    hibernate project first commit 9/26/2016

---
+++ Day31/src/ssa/Major.java
@@ -0,0 +1,62 @@
+package ssa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="major")
+public class Major {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	int id;
+	@Column(name="description")
+	String description;
+	@Column(name="req_sat")
+	int req_sat;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getReq_sat() {
+		return req_sat;
+	}
+
+	public void setReq_sat(int req_sat) {
+		this.req_sat = req_sat;
+	}
+
+	public Major(String description, int req_sat) {
+		super();
+		this.description = description;
+		this.req_sat = req_sat;
+	}
+	
+	public Major(){}
+	
+	@Override
+	public String toString() {
+		String msg = (String.format("%2d %-24s %4d", getId(),getDescription(),getReq_sat()));
+				return msg;
+	}
+}
---
+++ Day31/src/ssa/createMajor.java
@@ -0,0 +1,125 @@
+package ssa;
+
+
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class createMajor {
+
+	public static void main(String[] args) {
+		newMajor();
+		updateMajor();
+//  deleteMajor();
+		display();
+	}
+
+	
+	
+// create a new major in the major table	
+public static void newMajor(){
+	SessionFactory factory = new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Major.class)
+			.buildSessionFactory();
+	Session session = factory.getCurrentSession();
+	try{
+		Major maj1 = new Major("Pre-Law",1300);
+		Major maj2 = new Major("Communications",800);
+	
+		
+		//begin transaction
+		session.beginTransaction();
+		
+		session.save(maj1);
+		session.save(maj2);
+		
+		System.out.println("Created new majors");
+		session.getTransaction().commit();
+		
+	} catch(Exception ex){ ex.getStackTrace(); 
+		
+	} finally {
+		factory.close();
+	}
+}
+
+public static void updateMajor() {
+	SessionFactory factory = new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Major.class)
+			.buildSessionFactory();
+	Session session = factory.getCurrentSession();
+	try{		
+		//begin transaction
+		session.beginTransaction();
+//		session.createQuery("update Major set description = 'Beauty School' where id =7").executeUpdate();
+		session.createQuery("update Major set description = 'Sports Management' where id =1").executeUpdate();
+
+		session.getTransaction().commit();
+		System.out.println("update Major table");
+		
+	} catch(Exception ex){ ex.getStackTrace(); 
+		
+	} finally {
+		factory.close();
+	}
+}
+public static void deleteMajor() {
+	SessionFactory factory = new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Major.class)
+			.buildSessionFactory();
+	Session session = factory.getCurrentSession();
+	try{	
+		int majorId=9;
+		//begin transaction
+		session.beginTransaction();
+		Major major = session.get(Major.class,majorId);
+		
+		session.delete(major);
+		
+		System.out.println("delete major nursing");
+		session.getTransaction().commit();
+		
+	} catch(Exception ex){ ex.getStackTrace(); 
+		
+	} finally {
+		factory.close();
+	}
+}
+private static void display() {
+	SessionFactory factory = new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Major.class)
+			.buildSessionFactory();
+	Session session = factory.getCurrentSession();
+	try{	
+		session.beginTransaction();
+
+		List<Major> majors1= session.createQuery("from Major").list();
+		for(Major maj: majors1)
+			System.out.println(maj);
+
+		session.getTransaction().commit();
+		
+	} catch(Exception ex){ ex.getStackTrace(); 
+		
+	} finally {
+		factory.close();
+	}
+}
+
+
+
+}
+
+
+
+
+
+
